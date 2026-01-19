391408ad74d4dd969cbc8f477b81540fbbb8b8a5
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2026-01-19 20:56:23 +0800

    Upload files.

---
+++ main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+require("./server.js");
+
+let win;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 120,
+    title: "noname_websocket_server",
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => app.quit());
+
+app.on("activate", () => {
+	if (win === null) createWindow();
+});
